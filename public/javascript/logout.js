a4f98a963bfe996a9699ab47899c6dd88916400d
Buckimus <ryan42buck@gmail.com>
2022-08-25 18:07:55 -0400

    added logout

---
+++ public/javascript/logout.js
@@ -0,0 +1,11 @@
+async function logout() {
+  const response = await fetch("/api/users/logout", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    document.location.replace('/')
+  } else {
+    alert(response.statusText)
+  }
+}
