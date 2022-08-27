675db111eda243e1fe41778d70f26c42f2bdbbcc
Buckimus <ryan42buck@gmail.com>
2022-08-27 11:44:07 -0400

    added comments for clarification

--- public/javascript/addGame.js
+++ public/javascript/addGame.js
@@ -1,9 +1,12 @@
+
+//Handles event and will start all actions needed to create new game
 async function newGameHandler(event) {
   event.preventDefault();
 
   await createGame();
 }
 
+//Creates game and sends to post route
 async function createGame() {
   const name = document.querySelector("#game-name").value.trim();
   const description = document.querySelector("#game-description").value.trim();
@@ -34,16 +37,21 @@ async function createGame() {
     alert(response.statusText);
   }
 }
-
+//adds game to bridge route since users are allowed to pick multiple
 async function addToBridge(name) {
+
+    //empty array for choices to get pushed to
   let wantedCategories = [];
 
+  //checked boxes - still not set up correctly. Values will be send to empty array
   const categories = document.querySelector("#game-categories");
-
+    //gets id of created game
   const response = await fetch(`api/name/${name}`, {
     method: "get",
   });
   if (response.ok) {
+
+    //needs to be a for each/ each to work with each of the different categories the user picks
     const bridgeResponse = await fetch("/api/gtb", {
       method: "post",
       body: JSON.stringify({
