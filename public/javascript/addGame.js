0faf8d9262b5d1bc3dc6bfad1851f0bdc2869106
Buckimus <ryan42buck@gmail.com>
2022-08-27 14:50:02 -0400

    added some verification to addnewgame on the front end

--- public/javascript/addGame.js
+++ public/javascript/addGame.js
@@ -1,13 +1,50 @@
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+            newGameHandler();
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
 //Handles event and will start all actions needed to create new game
 
 async function newGameHandler(event) {
   event.preventDefault();
-  const name = document.querySelector("#game-name").value.trim();
-  const description = document.querySelector("#game-description").value.trim();
-  const min_players = document.querySelector("#game-min-player").value.trim();
-  const max_players = document.querySelector("#game-max-player").value.trim();
-  const duration = document.querySelector("#game-duration").value.trim();
-  const age_rating = document.querySelector("#game-age-rating").value.trim();
+  const name = document.querySelector('input[name="game-name]').value.trim();
+  const description = document
+    .querySelector('input[name="game-description]')
+    .value.trim();
+  const min_players = document
+    .querySelector('input[name="game-min-player]')
+    .value.trim();
+  const max_players = document
+    .querySelector('input[name="game-max-player]')
+    .value.trim();
+  const duration = document
+    .querySelector('input[name="game-duration]')
+    .value.trim();
+  const age_rating = document
+    .querySelector('input[name="game-age-rating]')
+    .value.trim();
   const allSelectedCategories = document.querySelectorAll(
     "input[type=checkbox]:checked"
   );
@@ -38,6 +75,6 @@ async function newGameHandler(event) {
   }
 }
 
-document
-  .querySelector("#submit-new-game")
-  .addEventListener("click", newGameHandler);
+// document
+//   .querySelector("#submit-new-game")
+//   .addEventListener("click", newGameHandler);
