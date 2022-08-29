797d8b760382af9836ea860bd7398aa03defa27e
Buckimus <ryan42buck@gmail.com>
2022-08-28 21:20:11 -0400

    added user seed

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const Category = require("./Category");
 const Review = require("./Review");
 const Game = require("./Game");
 const GameList = require("./GameList");
-const game_category_bridge = require('./game_category_bridge');
+const game_category_bridge = require("./game_category_bridge");
 
 User.hasMany(Review);
 User.hasMany(Game);
-User.hasOne(GameList, {onDelete: 'cascade'});
+User.hasMany(GameList);
 
 Review.belongsTo(User);
 Review.belongsTo(Game);
@@ -15,10 +15,18 @@ Review.belongsTo(Game);
 Game.hasMany(Review);
 Game.belongsToMany(Category, { through: game_category_bridge });
 Game.belongsTo(User);
+Game.hasMany(GameList);
 
 Category.belongsToMany(Game, { through: game_category_bridge });
 
 GameList.belongsTo(User);
 GameList.hasMany(Game);
 
-module.exports = { User, Category, Review, Game, GameList, game_category_bridge };
+module.exports = {
+  User,
+  Category,
+  Review,
+  Game,
+  GameList,
+  game_category_bridge,
+};
--- seed/index.js
+++ seed/index.js
@@ -3,9 +3,12 @@ const seedCategory = require("./seedCategory");
 const seedGame = require("./seedGame");
 const seedGameCategoryBridge = require("./seedGameCategoryBridge");
 const seedGameList = require("./seedGameList");
+const seedUser = require("./seedUser");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+
+  await seedUser();
 
   await seedCategory();
 
