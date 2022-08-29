348e41c3036fd7f6b7e1eb169c3a47e35f8699b3
Buckimus <ryan42buck@gmail.com>
2022-08-28 22:34:29 -0400

    created routes to render categories

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const session = require("express-session");
-const { Game, User, Category } = require("../models");
+const { Game, User, Category, game_category_bridge } = require("../models");
 
 const router = require("express").Router();
 
@@ -20,13 +20,55 @@ router.get("/login", (req, res) => {
   res.render("login", { loggedIn: req.session.loggedIn });
 });
 
-
 router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
   res.render("signup", { loggedIn: req.session.loggedIn });
+});
+
+// router.get('/game', async (req,res) => {
+//   const allGames = Game.findAll({
+//     attributes: ['id', 'name', 'description']
+//   })
+// })
+
+router.get("/category", async (req, res) => {
+  try {
+    const allCategories = await Category.findAll({
+      attributes: ["id", "category_name"],
+    });
+    const categories = allCategories.map((category) =>
+      category.get({ plain: true })
+    );
+    res.render("categories", { categories, loggedIn: req.session.loggedIn });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+router.get("/category/:id", async (req, res) => {
+  try {
+    const catGames = await Category.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "category_name"],
+      include: {
+        model: Game,
+        attributes: ["id", "name", "description"],
+        through: game_category_bridge,
+      },
+    });
+    const category = catGames.get({ plain: true });
+    console.log(category);
+    res.render("single-category", { category });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
