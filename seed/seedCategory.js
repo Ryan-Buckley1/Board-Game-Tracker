773dc7536f08142c7c7361271b87e3fd62626d76
Ryan Dooley <Rdoolz51@aol.com>
2022-08-30 20:56:24 -0400

    seed data for categories

--- seed/seedCategory.js
+++ seed/seedCategory.js
@@ -1,22 +1,88 @@
-const {Category} = require('../models')
+const { Category } = require('../models');
 
 const categoryData = [
     {
-        category_name: 'trivia'
+        category_name: 'Trivia'
     },
     {
-        category_name: 'card'
+        category_name: 'Card'
     },
     {
-        category_name: 'war'
+        category_name: 'War'
     },
     {
-        category_name: 'drawing'
+        category_name: 'Drawing'
     },
     {
-        category_name: 'strategy'
+        category_name: 'Sports'
     },
-]
-const seedCategory = () => Category.bulkCreate(categoryData)
+    {
+        category_name: 'Comedy'
+    },
+    {
+        category_name: 'Dice'
+    },
+    {
+        category_name: 'Horror'
+    },
+    {
+        category_name: 'Fighting'
+    },
+    {
+        category_name: 'Math'
+    },
+    {
+        category_name: 'Mature/Adult'
+    },
+    {
+        category_name: 'Anime'
+    },
+    {
+        category_name: 'Puzzle'
+    },
+    {
+        category_name: 'Fantasy'
+    },
+    {
+        category_name: 'Medical'
+    },
+    {
+        category_name: 'Exploration'
+    },
+    {
+        category_name: 'Novel-based'
+    },
+    {
+        category_name: 'Party Game'
+    },
+    {
+        category_name: 'Miniatures'
+    },
+    {
+        category_name: 'Word Game'
+    },
+    {
+        category_name: 'Educational'
+    },
+    {
+        category_name: 'Tabletop'
+    },
+    {
+        category_name: 'Mafia'
+    },
+    {
+        category_name: 'Memory'
+    },
+    {
+        category_name: 'Racing'
+    },
+    {
+        category_name: 'Multilingual'
+    },
+    {
+        category_name: 'Expansion'
+    },
+];
+const seedCategory = () => Category.bulkCreate(categoryData);
 
-module.exports = seedCategory
+module.exports = seedCategory;
