6ea6a6ba351cd92d859bd6ee7a601a7e3cb56722
Ryan Dooley <Rdoolz51@aol.com>
2022-09-01 03:21:44 -0400

    commented out Review Models in associations

--- models/index.js
+++ models/index.js
@@ -5,14 +5,14 @@ const Game = require("./Game");
 const GameList = require("./GameList");
 const game_category_bridge = require("./game_category_bridge");
 
-User.hasMany(Review);
+// User.hasMany(Review);
 User.hasMany(Game);
 User.hasMany(GameList, { foreignKey: "user_id" });
 
 // Review.belongsTo(User); FUTURE DEVELOPMENT
 // Review.belongsTo(Game); FUTURE DEVELOPMENT
 
-Game.hasMany(Review);
+// Game.hasMany(Review);
 Game.belongsToMany(Category, { through: game_category_bridge });
 Game.hasMany(GameList, { foreignKey: "game_id" });
 
