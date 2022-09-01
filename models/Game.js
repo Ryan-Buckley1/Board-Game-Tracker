05e6d216dc3e6d74b3c26e54f1beb7f225d1a708
Ryan Dooley <Rdoolz51@aol.com>
2022-09-01 12:03:12 -0400

    ui work

--- models/Game.js
+++ models/Game.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Game extends Model {}
+class Game extends Model { }
 
 Game.init(
   {
